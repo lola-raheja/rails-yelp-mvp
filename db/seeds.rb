58dfd40b221907c747863e8a16a460824f21c59d
Lola Boatwright <lolaboatwright@gmail.com>
2023-01-24 21:28:29 +0000

    added seed data and started to configure routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating restaurants..."
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '555-555-5555' , category: 'french' }
+raunka_punjab = { name: 'Raunka Punjab', address: 'Southall', phone_number: '555-555-5555' , category: 'chinese' }
+nambu_tei = { name: 'Nambu-tei', address: 'Baker Street', phone_number: '555-555-5555' , category: 'japanese' }
+corazon = { name: 'Corazon', address: 'Oxford Circus', phone_number: '555-555-5555' , category: 'belgian' }
+homeslice = { name: 'Homeslice', address: 'James Street', phone_number: '555-555-5555', category: 'italian' }
+
+[dishoom, raunka_punjab, nambu_tei, corazon, homeslice].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
